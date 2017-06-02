7ac1b2a3d5fa9861715bad5e80c123d6d8168e89
jenaArpita <arpita93@yahoo.com>
2017-06-02 11:48:26 +0530

    Initial commit

---
+++ app/src/main/java/com/atrio/demoradiobutton/MainActivity.java
@@ -0,0 +1,33 @@
+package com.atrio.demoradiobutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<String> arr_qty;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        arr_qty=new ArrayList<>();
+        for(int i =0;i<50;i++){
+            arr_qty.add(String.valueOf(i));
+
+        }
+        RecyclerView dummy = (RecyclerView)findViewById(R.id.dummy_recycle);
+        DummyAdapter adapter = new DummyAdapter(getApplicationContext(), arr_qty);
+        dummy.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
+        dummy.setAdapter(adapter);
+    }
+
+    void senddata(){
+        Log.i("hello",""+"hii");
+    }
+}
