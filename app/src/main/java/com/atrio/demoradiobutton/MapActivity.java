2f38c6bf0b8970f1f3303e95000c67c8e54c7d0e
jenaArpita <arpita93@yahoo.com>
2017-06-05 10:46:08 +0530

    Merge remote-tracking branch 'remotes/origin/Branch02'
    # Conflicts:
    #	app/src/main/java/com/atrio/demoradiobutton/MapActivity.java

--- app/src/main/java/com/atrio/demoradiobutton/MapActivity.java
+++ app/src/main/java/com/atrio/demoradiobutton/MapActivity.java
@@ -2,6 +2,7 @@ package com.atrio.demoradiobutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MapActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        fetchData();
     }
+
+    private void fetchData() {
+
+        Log.i("hii",""+"coming");
+    }
+
+
 }
